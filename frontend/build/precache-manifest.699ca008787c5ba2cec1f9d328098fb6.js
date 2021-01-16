bac1e97c956f3f8cebd52a1221007a53265931db
Al-Parillon <alparillon@gmail.com>
2021-01-16 01:44:06 -0800

    the project

---
+++ frontend/build/precache-manifest.699ca008787c5ba2cec1f9d328098fb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f193229c3232c8739238fe697effbaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f29d8c00dbb06b53c7ee",
+    "url": "/static/css/main.503ef885.chunk.css"
+  },
+  {
+    "revision": "4a74ef7916c8fd9d4b0c",
+    "url": "/static/js/2.043924fb.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.043924fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f29d8c00dbb06b53c7ee",
+    "url": "/static/js/main.283d210b.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
